1f46de57551ee64f231b02ae6daa8e9e101cfe40
Vignesh1625 <112402061+Vignesh1625@users.noreply.github.com>
2023-05-14 08:55:26 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+const yesBtn = document.getElementById("yes-btn");
+const noBtn = document.getElementById("no-btn");
+
+// Add click event listeners to the buttons
+yesBtn.addEventListener("click", function() {
+  alert("Great! Baby Let's hang out.");
+});
+
+noBtn.addEventListener("click", function() {
+  alert("Okay, maybe some other time.");
+});
